80f51df646c50f0f193be7889c8f00286e8076ee
Stephanie Delgado <stephaniedelgado@Stephanies-Air.(none)>
2022-09-20 15:40:40 -0400

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BookList from './components/BookList';
+import Home from './components/Home';
 
 function App() {
-  // return (
+  return (
     
-  // );
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<BookList />}/>
+          
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
