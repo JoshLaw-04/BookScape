7375165bef26aba67086b478d34c818b42a9ff3c
sundogjoshua <100004343+sundogjoshua@users.noreply.github.com>
2022-09-23 10:09:02 -0800

    Update SignUp.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Form, FormControl } from "react-bootstrap";
+import Stack from "react-bootstrap/Stack";
+import { Link, Outlet } from "react-router-dom";
+import "./Home.css";
+
+function SignUp() {
+
+  return (
+    <>
+      <Navbar className="Navbar">
+        <Navbar.Brand className="logo">
+          <img
+            alt=""
+            // src={}
+            width="45"
+            height="45"
+            style={{ padding: "5px" }}
+          />{" "}
+          Bookscape
+        </Navbar.Brand>
+        <Container className="justify-content-end">
+          <Nav>
+            <Link to="/" className="nav-link c-white">
+                Home
+            </Link>
+            <Link to="/about" className="nav-link">
+                About
+            </Link>
+            <Link to="/register" className="nav-link">
+                Register
+            </Link>
+            <Link to="/login" className="nav-link">
+                Login
+            </Link>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="search"
+                // onChange={filterSearch}
+              />
+            </Form>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <div>
+
+<form id="FormSignIn">
+<label>Email :
+  <input type="text" />
+</label>
+<br></br><br></br>
+<label>Username :
+  <input type="text" />
+</label>
+<br></br><br></br>
+<label>Password :
+  <input type="text" />
+</label>
+<br></br><br></br>
+<label>First Name :
+  <input type="text" />
+</label>
+<br></br><br></br>
+<label>Last Name :
+  <input type="text" />
+</label>
+<br></br><br></br>
+<input type="submit" value="Sign In" />
+</form>
+
+</div>
+
+      <Stack>
+        <Outlet />
+      </Stack>
+      {/* <footer className="footer">
+        <div className="footer-copyright text-center py-3">
+          © 2022 Bookscape • Website Design by Team Chosen
+        </div>
+      </footer> */}
+    </>
+  );
+}
+
+export default SignUp;
