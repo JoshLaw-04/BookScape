cb6ecaf067a97879ba182e5ba3f88d55fab31122
sundogjoshua <100004343+sundogjoshua@users.noreply.github.com>
2022-09-23 10:07:58 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ import { Form, FormControl } from "react-bootstrap";
 import Stack from "react-bootstrap/Stack";
 import { Link, Outlet } from "react-router-dom";
 import "./Home.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function Home() {
 
@@ -46,6 +50,147 @@ function Home() {
           </Nav>
         </Container>
       </Navbar>
+
+<br></br>
+
+      <div id="CardsCol">
+      
+      <Container>
+      <Row>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+      </Row>
+      <br></br>
+      <Row>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+      </Row>
+<br></br>
+      <Row>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+        <Col>
+        
+        <Card id="CardBorder" style={{ width: '245px' }}>
+      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Author
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+        </Col>
+
+      </Row>
+      
+    </Container>
+
+      </div>
 
       <Stack>
         <Outlet />
