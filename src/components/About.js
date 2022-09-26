5f5347d32c808d62f601c44975b9bbb4018496c8
Stephanie Delgado <stephaniedelgado@Stephanies-Air.(none)>
2022-09-26 16:26:58 -0400

    Adjustment to BookList and refining about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-
+import bill from './assets/bill.jpg'
+import stephanie from './assets/stephanie.jpg'
 
 function About() {
 
     const team = [
-        {name: 'Bill Franklin', role: 'Backend Developer'},
-        {name: 'Katie Watson', role: 'Backend Developer'},
-        {name: 'Stephanie Delgado', role: 'Frontend Developer'},
-        {name: 'Josh Law', role: 'Frontend Developer'},
-        {name: 'Caleb Smith', role: 'Frontend Developer'}
+        {
+            name: 'Bill Franklin', 
+            role: 'Backend Developer',
+            about: 'A fermented sausage and traditional cured meat enthusiast that loves to code, lead, learn, create, and problem solve.  His true heart is to see each and every person he meets achieve the best version of themselves and to see transformation and achievement of new heights professionally and personally.  He loves to encourage with words of affirmation and the challenge to understand each person’s unique situation.  The developing world has opened him up to new ways to create and challenge himself adding a dimension of satisfaction that has been non-existent in his life before now.',
+            image: bill
+        },
+        {
+            name: 'Katie Watson', 
+            role: 'Backend Developer'
+        },
+        {
+            name: 'Stephanie Delgado', 
+            role: 'Frontend Developer',
+            about: 'A problem solver with a love for research. Stephanie started learning to code in 2019, though it began as more of a hobby than a career focus. After a college education in Psychology and continuing to pursue that hobby by enlisting in a coding Bootcamp in 2021, Stephanie discovered another passion, her love for coding. Her ultimate goal is to utilize her knowledge and experience in behavioral health and her new software skills to continue serving underserved communities.', 
+            image: stephanie
+        },
+        {
+            name: 'Josh Law', 
+            role: 'Frontend Developer'
+        },
+        {
+            name: 'Caleb Smith', 
+            role: 'Frontend Developer'
+        }
     ]
 
     const displayTeam = () => {
-        return team.map(({name, role}, i) => {
+        return team.map(({name, role, image,about}, i) => {
             return (
-                <Card style={{ width: '25em', margin: '15px' }} key={i}>
+                <Card style={{ width: '45em', margin: '15px' }} key={i}>
                     <Row>
                         <Col xs='3' sm='3' md='3' lg='4' xl='4'>
-                            <Card.Img variant="top" src="holder.js/100px180" style={{padding: '5px'}}/>
+                            <Card.Img variant="top" src={image} style={{padding: '5px'}}/>
                         </Col>
                         <Col xs='9' sm='9' md='9' lg='8' xl='8'>
                             <Card.Body>
                                 <Card.Title>{name}</Card.Title>
                                 <Card.Text>
                                     {role}
+                                    {about}
                                 </Card.Text>
                             </Card.Body>
                         </Col>
@@ -37,11 +58,11 @@ function About() {
   return (
     <Container>
         <Row style={{paddingTop: '15px'}}>
-            <Col xs='12' sm='12' md='6' lg='6' xl='6'>
+            <Col xs='12' sm='12' md='6' lg='6' xl='5'>
                 <h2 style={{paddingBottom: '15px'}}>About Us</h2>
-                <p style={{paddingRight: '20px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget egestas purus viverra accumsan in nisl nisi. Morbi blandit cursus risus at ultrices mi tempus imperdiet. At risus viverra adipiscing at. Tincidunt dui ut ornare lectus sit amet est. Nunc mattis enim ut tellus elementum. Amet consectetur adipiscing elit pellentesque. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Velit sed ullamcorper morbi tincidunt ornare massa. Sodales ut etiam sit amet nisl. Commodo odio aenean sed adipiscing. Eu ultrices vitae auctor eu augue ut. Interdum posuere lorem ipsum dolor sit amet consectetur. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor.</p>
+                <p style={{paddingRight: '20px'}}>Our team is a talented, cross-generational, multi-ethnical group of leaders that strives for excellence in all arenas of life.  This unique combination of drive and determination makes our projects come alive individually and corporately.  This team also represents humility and the authentic learner spirit, which has contributed immensely to this project's collaborative nature and provides a shining example of working together.  Each team member exemplifies the highest moral and ethical standard that allows the individual to stand out in any circle.  We were handpicked for this project which led to the name Team Chosen.  Without any further ado, please meet the talented individuals that make up our team.</p>
             </Col>
-            <Col xs='12' sm='12' md='6' lg='6' xl='6'>
+            <Col xs='12' sm='12' md='6' lg='6' xl='7'>
                 <h2 style={{paddingBottom: '15px', paddingLeft: '15px'}}>Team Chosen</h2>
                 {displayTeam()}
             </Col>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,8 @@ import { Form, FormControl } from "react-bootstrap";
 import Stack from "react-bootstrap/Stack";
 import { Link, Outlet } from "react-router-dom";
 import "./Home.css";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import logo from './assets/Bookscape.png'
+
 
 function Home() {
 
@@ -17,9 +15,9 @@ function Home() {
         <Navbar.Brand className="logo">
           <img
             alt=""
-            // src={}
-            width="45"
-            height="45"
+            src={logo}
+            width="50"
+            height="50"
             style={{ padding: "5px" }}
           />{" "}
           Bookscape
@@ -50,147 +48,6 @@ function Home() {
           </Nav>
         </Container>
       </Navbar>
-
-<br></br>
-
-      <div id="CardsCol">
-      
-      <Container>
-      <Row>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-      </Row>
-      <br></br>
-      <Row>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-      </Row>
-<br></br>
-      <Row>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-        <Col>
-        
-        <Card id="CardBorder" style={{ width: '245px' }}>
-      <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Title</Card.Title>
-        <Card.Text>
-          Author
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-        </Col>
-
-      </Row>
-      
-    </Container>
-
-      </div>
 
       <Stack>
         <Outlet />
