efb4dd387183a8d51ef7384348efd73439101487
sundogjoshua <100004343+sundogjoshua@users.noreply.github.com>
2022-10-02 12:45:49 -0800

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Home.css";
+import axios from "axios";
 
-function SignIn() {
+const baseUrl = "http://localhost:3000/api/users/";
+
+function signInUser(username, password) {
+  let user = { username, password };
+
+  return axios.post(`${baseUrl}/register`, user)
+      .then(response => {
+          localStorage.setItem('signUserToken', response.data.token)
+          return new Promise(resolve => resolve(response.data));
+      }
+  );
+}
+
+
+const SignIn = () => {
+
+  const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      signInUser(username, password).then(() => {
+      }).catch(error => {
+          console.log(error);
+          window.alert('Failed login');
+      });
+  }
 
   return (
     <>
-      <div id="FormSignIn">
+      <div id="FormSignIn" onSubmit={handleSubmit}>
         <form>
           <label>Username :
-            <input type="text" />
+            <input placeholder="Enter username" type="text" name="username" onChange={e => setUsername(e.target.value)} />
           </label>
           <br></br><br></br>
           <label>Password :
-            <input type="text" />
+            <input placeholder="Enter password" type="text" name="password" onChange={e => setPassword(e.target.value)} />
           </label>
           <br></br><br></br>
             <input type="submit" value="Sign In" />
@@ -20,6 +50,6 @@ function SignIn() {
       </div>
     </>
   );
-}
+  }
 
 export default SignIn;
