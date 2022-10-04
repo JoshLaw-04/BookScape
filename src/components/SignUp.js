667353808caae0d5a80c597169cbde47e887117d
Stephanie Delgado <stephaniedelgado@Stephanies-Air.(none)>
2022-10-03 22:35:42 -0400

    updated register

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,63 +1,111 @@
-import React, { useContext, useState } from 'react';
-import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import UserContext from '../contexts/UserContext';
+// import React, { useContext, useState } from 'react';
+// import { Button } from 'react-bootstrap';
+// import { useNavigate } from 'react-router-dom';
+// import UserContext from '../contexts/UserContext';
 import "./Home.css";
 
-const SignUp =() => {
+// const SignUp =() => {
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const [email, setEmail] = useState("");
 
-  let { createUser } = useContext(UserContext);
-  let navigate = useNavigate();
+//   let { createUser } = useContext(UserContext);
+//   let navigate = useNavigate();
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    createUser(username, password, firstName, lastName, email).then(() => {
-      navigate('/login');
-    }).catch(error => {
-        console.log(error);
-        window.alert('Failed registration: error creating user');
-    });
-  }
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     createUser(username, password, firstName, lastName, email).then(() => {
+//       navigate('/login');
+//     }).catch(error => {
+//         console.log(error);
+//         window.alert('Failed registration: error creating user');
+//     });
+//   }
 
 
-  return (
-    <>
-    <form onSubmit={handleSubmit}>
-        <h1 style={{paddingTop: '30px', paddingBottom: '15px'}}>REGISTER</h1>
-        <span style={{paddingRight: '14px'}}>Username  </span>
-        <input placeholder="Enter Username" type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
-        <br></br><br></br>
-        <span style={{paddingRight: '18px'}}>Password  </span>
-        <input placeholder="Enter Password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-        <br /><br></br>
-        <span style={{paddingRight: '10px'}}>First Name  </span>
-        <input placeholder="Enter First Name" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} />
-        <br /><br></br>
-        <span style={{paddingRight: '10px'}}>Last Name  </span>
-        <input placeholder="Enter Last Name" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} />
-        <br /><br></br>
-        <span style={{paddingRight: '50px'}}>Email  </span>
-        <input placeholder="Enter Email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-        <br /><br></br>
-        <Button type='submit' style={{backgroundColor: '#000807', color: '#A2A3BB', marginBottom: '5px', marginTop: '15px'}}>Sign Up</Button>
-    </form>
-    <div style={{paddingBottom:'230px'}}>
+//   return (
+//     <>
+//     <form onSubmit={handleSubmit}>
+//         <h1 style={{paddingTop: '30px', paddingBottom: '15px'}}>REGISTER</h1>
+//         <span style={{paddingRight: '14px'}}>Username  </span>
+//         <input placeholder="Enter Username" type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
+//         <br></br><br></br>
+//         <span style={{paddingRight: '18px'}}>Password  </span>
+//         <input placeholder="Enter Password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+//         <br /><br></br>
+//         <span style={{paddingRight: '10px'}}>First Name  </span>
+//         <input placeholder="Enter First Name" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} />
+//         <br /><br></br>
+//         <span style={{paddingRight: '10px'}}>Last Name  </span>
+//         <input placeholder="Enter Last Name" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} />
+//         <br /><br></br>
+//         <span style={{paddingRight: '50px'}}>Email  </span>
+//         <input placeholder="Enter Email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+//         <br /><br></br>
+//         <Button type='submit' style={{backgroundColor: '#000807', color: '#A2A3BB', marginBottom: '5px', marginTop: '15px'}}>Sign Up</Button>
+//     </form>
+//     <div style={{paddingBottom:'230px'}}>
 
-    </div>
-    </>
-  )
+//     </div>
+//     </>
+//   )
  
  
   
-}
+// }
 
- 
+
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
+
+const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    let { createUser } = useContext(UserContext);
+    let navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        createUser(username, password, firstName, lastName, email).then(() => {
+            navigate('/login');
+        }).catch(error => {
+            console.log(error);
+            window.alert('Failed registration: error creating user');
+        });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>REGISTER</h1>
+            <br></br><br></br>
+            <span>Username  </span>
+            <input placeholder="Enter Username" type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
+            <br></br><br></br>
+            <span>Password  </span>
+            <input placeholder="Enter Password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <br /><br></br>
+            <span>First Name  </span>
+            <input placeholder="Enter First Name" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} />
+            <br /><br></br>
+            <span>Last Name  </span>
+            <input placeholder="Enter Last Name" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} />
+            <br /><br></br>
+            <span>Email  </span>
+            <input placeholder="Enter Email" type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <br /><br></br>
+            <button>Sign Up</button>
+        </form>
+    )
+};
+
 export default SignUp;
  
  
--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -1,23 +1,92 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import UserContext from "./UserContext";
+
+// export const UserProvider = (props) => {
+
+//     const [ users, setUsers ] = useState([]);
+//     const baseUrl = "http://localhost:3000/api/users/";
+
+//     // useEffect(() => {
+//     //     const loggedInUser = localStorage.getItem('user');
+//     //     if (loggedInUser) {
+//     //         const foundUser = JSON.parse(loggedInUser);
+//     //         setUsers(foundUser);
+//     //     }
+//     // }, []);
+
+//     function createUser(newUser) {
+        
+//         return axios.post(baseUrl, newUser)
+//             .then(response => {
+//                 return new Promise(resolve => resolve(response.data));
+//             }
+//         );
+//     }
+
+//     function signInUser(username, password) {
+//         let user = { username, password };
+
+//         return axios.post(`${baseUrl}login`, user)
+//             .then(response => {
+//                 setUsers(response.data.existingUser)
+//                 localStorage.setItem('myToken', response.data.token)
+//                 localStorage.setItem('user', JSON.stringify(response.data.existingUser))
+//                 return new Promise(resolve => resolve(response.data));
+//             }
+//         );
+//     }
+
+//     function getUserProfile(id) {
+//         let myHeaders = {
+//             Authorization: `Bearer ${localStorage.getItem('myToken')}` 
+//         };
+        
+//         return axios.get(`${baseUrl}profile/` + id, { headers: myHeaders })
+//             .then(response => {
+//                 return new Promise((resolve) => resolve(response.data));
+//             }
+//         );
+//     }
+
+//     // function editUser(user) {
+//     //     let myHeaders = {
+//     //         Authorization: `Bearer ${localStorage.getItem('myToken')}`
+//     //     };
+
+//     //     return axios.put(`${baseUrl}profile/edit/` + user.userId, user, { headers: myHeaders })
+//     //         .then(response => {
+//     //             getUserProfile(user.userId);
+//     //             return new Promise(resolve => resolve(response.data));
+//     //         }
+//     //     );
+//     // }
+
+
+//     return (
+//         <UserContext.Provider value={{
+//             users,
+//             createUser,
+//             signInUser,
+//             getUserProfile
+//         }}>
+//             { props.children }
+//         </UserContext.Provider>
+//     )
+// }
+
+
 import axios from "axios";
-import { useEffect, useState } from "react";
 import UserContext from "./UserContext";
 
 export const UserProvider = (props) => {
 
-    const [ users, setUsers ] = useState([]);
     const baseUrl = "http://localhost:3000/api/users/";
 
-    // useEffect(() => {
-    //     const loggedInUser = localStorage.getItem('user');
-    //     if (loggedInUser) {
-    //         const foundUser = JSON.parse(loggedInUser);
-    //         setUsers(foundUser);
-    //     }
-    // }, []);
-
-    function createUser(newUser) {
+    function createUser(username, password) {       
+        let user = { username, password };
         
-        return axios.post(baseUrl, newUser)
+        return axios.post(baseUrl, user)
             .then(response => {
                 return new Promise(resolve => resolve(response.data));
             }
@@ -26,49 +95,19 @@ export const UserProvider = (props) => {
 
     function signInUser(username, password) {
         let user = { username, password };
-
-        return axios.post(`${baseUrl}login`, user)
+    
+        return axios.post(`${baseUrl}/login`, user)
             .then(response => {
-                setUsers(response.data.existingUser)
                 localStorage.setItem('myToken', response.data.token)
-                localStorage.setItem('user', JSON.stringify(response.data.existingUser))
                 return new Promise(resolve => resolve(response.data));
             }
         );
     }
 
-    function getUserProfile(id) {
-        let myHeaders = {
-            Authorization: `Bearer ${localStorage.getItem('myToken')}` 
-        };
-        
-        return axios.get(`${baseUrl}profile/` + id, { headers: myHeaders })
-            .then(response => {
-                return new Promise((resolve) => resolve(response.data));
-            }
-        );
-    }
-
-    // function editUser(user) {
-    //     let myHeaders = {
-    //         Authorization: `Bearer ${localStorage.getItem('myToken')}`
-    //     };
-
-    //     return axios.put(`${baseUrl}profile/edit/` + user.userId, user, { headers: myHeaders })
-    //         .then(response => {
-    //             getUserProfile(user.userId);
-    //             return new Promise(resolve => resolve(response.data));
-    //         }
-    //     );
-    // }
-
-
     return (
         <UserContext.Provider value={{
-            users,
             createUser,
-            signInUser,
-            getUserProfile
+            signInUser
         }}>
             { props.children }
         </UserContext.Provider>
