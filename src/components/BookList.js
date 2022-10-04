33e6af01243ff5d79023d786c8007be26f497339
hellok8ie <k8iewatson@yahoo.com>
2022-10-04 17:52:00 -0400

    Search results being mapped and displaying title and image on cards

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,176 +5,209 @@ import BookContext from "../contexts/BookContext";
 
 function BookList() {
 
-  let { searchResults } = useContext(BookContext);
   let [bookItem, setBookItem] = useState();
 
   return (
-
-    <>
-            {
-              searchResults.map((item) => {
-                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-                    let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-                    if(thumbnail!== undefined && amount !==undefined)
-                    {
-                        return (
-                            <>
-                            <div className="card" onClick={() =>{setBookItem(item)}}>
-                                <img src={thumbnail} alt="" />
-                                <div className="bottom">
-                                    <h3 className="title">{item.volumeInfo.title}</h3>
-                                    <p className="amount">${amount}</p>
-                                </div>
-                            </div>
-                            </>
-                        )
-                    }
-                    
-                })
-            }
-
-        </>
-
-
-  )
-
-
-
-
-
-
-function BookList() {
-
-    return (
-        <div id="CardsCol">
-      
-        <Container>
-            <Row>
-
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                                <Card.Text>
-                                    Author
-                                </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-            </Row>
-            <br></br>
-            <Row>
-
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-            </Row>
-            <br></br>
-            <Row>
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-                <Col>
-                    <Card id="CardBorder" style={{ width: '245px' }}>
-                        <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                                Author
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-    
-            </Row>
+    <BookContext.Consumer>
+    {
+        ({searchResults}) => {
             
-        </Container>
+            return (
+                <Container>
+                <h1>Book Results</h1>
+                <div id="CardsCol">
+                        <Row>
+                            {searchResults.map((book) => {
+                                let bookPic = book.volumeInfo.imageLinks.thumbnail
+                                return (
+
+                                    <Card id="CardBorder" style={{ width: '245px' }}>
+                                        <Card.Img variant="top" src={bookPic} />
+                                            <Card.Body>
+                                                <Card.Title>{book.volumeInfo.title}</Card.Title>
+                                                {/* {book.authors.map((author) => {
+                                                    return (<Card.Text>{author}</Card.Text>)})} */}
+                                            </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </Row>
+                </div>
+                </Container>
+            )
+
+        }
+
+
+
+    }
+    </BookContext.Consumer>
+)
+
+    //         {
+    //           searchResults.map((item) => {
+    //                 let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+    //                 let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+    //                 if(thumbnail!== undefined && amount !==undefined)
+    //                 {
+    //                     return (
+    //                         <>
+    //                         <div className="card" onClick={() =>{setBookItem(item)}}>
+    //                             <img src={thumbnail} alt="" />
+    //                             <div className="bottom">
+    //                                 <h3 className="title">{item.volumeInfo.title}</h3>
+    //                                 <p className="amount">${amount}</p>
+    //                             </div>
+    //                         </div>
+    //                         </>
+    //                     )
+    //                 }
+                    
+    //             })
+    //         }
+
+    //     </>
+
+
+
+
+}
+
+
+
+
+// function BookList() {
+
+//     return (
+//         <div id="CardsCol">
+      
+//         <Container>
+//             <Row>
+
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                                 <Card.Text>
+//                                     Author
+//                                 </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//             </Row>
+//             <br></br>
+//             <Row>
+
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//             </Row>
+//             <br></br>
+//             <Row>
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+
+//                 <Col>
+//                     <Card id="CardBorder" style={{ width: '245px' }}>
+//                         <Card.Img variant="top" src="holder.js/30px30?text=Image cap" />
+//                         <Card.Body>
+//                             <Card.Title>Title</Card.Title>
+//                             <Card.Text>
+//                                 Author
+//                             </Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+    
+//             </Row>
+            
+//         </Container>
   
-        </div>
-    )
+//         </div>
+//     )
 
   
-}
+// }
 
 
 export default BookList;
