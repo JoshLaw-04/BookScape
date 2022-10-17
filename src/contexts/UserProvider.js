62154a7990ee73b12c70a7a2c888a51d6d96972f
hellok8ie <k8iewatson@yahoo.com>
2022-10-17 18:18:21 -0400

    took out the refresh definition

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -53,7 +53,6 @@ export const UserProvider = (props) => {
     function signOutUser() {
         setisLoggedIn(false);
         localStorage.clear()
-        refresh();
     }
 
     function getUserProfile(id) {
